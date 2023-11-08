b2332f612196cdd92e6fbe8ca0bc6090872b2218
hkmshb <s.abdulhakeeem@gmail.com>
2023-11-08 06:43:18 +0100

    convert eslint-config-eha devDependencies to dependencies

--- packages/eslint-config-eha/index.js
+++ packages/eslint-config-eha/index.js
@@ -27,7 +27,7 @@ module.exports = {
     "@typescript-eslint/no-empty-interface": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": ["off", {argsIgnorePattern: "^_"}],
-    "max-len": ["error, {code: 100}"],
+    "max-len": ["error", {code: 100}],
     semi: ["error", "always"],
     "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error",
